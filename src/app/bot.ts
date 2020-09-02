101a9c4cc6c2ac22a66d5ebccb60d340898333c3
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-09-02 18:33:18 -0400

    feat(app): discord login

--- src/app/bot.ts
+++ src/app/bot.ts
@@ -1,6 +1,20 @@
+import { Client, Message, Guild, GuildMember, PartialGuildMember } from "discord.js";
+import messageHandler from "../handlers/message.handler";
+
 
 async function main(){
   
+  const bot: Client = new Client();
+  const token: string | undefined = process.env['DISCORD_TOKEN'];
 
+  bot.on("message", (message: Message) => messageHandler(message));
+
+  try {
+    await bot.login(token);
+    console.log("Ayudatina is online");
+  } 
+  catch (error) {
+    console.log("Login error");
+  }
 }
 export default { main };
